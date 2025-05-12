6b0a1c0b26f39c8e322f37d30d6b6c9ec7762b6d
Elias Zebene <zebene.elias@hotmail.com>
2025-05-12 23:43:44 +0100

    Final Test

---
+++ assets/js/config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+    UNSPLASH_API_KEY: 'jbLyVpcZxCaNeFlbcRQ5cH_GsL8NzmotumC-RvkyZtw', // Replace with your actual Unsplash API key
+};
+
+// Prevent modification of the config
+Object.freeze(CONFIG);
