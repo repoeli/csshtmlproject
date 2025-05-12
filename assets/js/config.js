df866b833bd57b0984a6fe52ab1dc42e16010791
Elias Zebene <zebene.elias@hotmail.com>
2025-05-12 23:46:25 +0100

    Remove configuration file for Unsplash API key to enhance security

--- assets/js/config.js
+++
@@ -1,6 +0,0 @@
-const CONFIG = {
-    UNSPLASH_API_KEY: 'jbLyVpcZxCaNeFlbcRQ5cH_GsL8NzmotumC-RvkyZtw', // Replace with your actual Unsplash API key
-};
-
-// Prevent modification of the config
-Object.freeze(CONFIG);
